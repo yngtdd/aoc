3e4066c01677bd280674c1bdaccb6660aea62a19
yngtdd <yngtdd@gmail.com>
2022-12-09 09:49:00 -0500

    refactor: clean up sorting in part 2

--- src/day01.rs
+++ src/day01.rs
@@ -4,9 +4,9 @@ type Elf = u32;
 pub fn input_generator(input: &str) -> Vec<Elf> {
     input
         .split("\n\n")
-        .map(|food| {
-            food.lines()
-                .map(|item| item.parse::<u32>().unwrap())
+        .map(|bag| {
+            bag.lines()
+                .filter_map(|food| food.parse::<u32>().ok())
                 .sum::<u32>()
         })
         .collect()
@@ -17,15 +17,12 @@ pub fn solve_part1(input: &[Elf]) -> u32 {
     let mut elves = input.to_vec();
     elves.sort();
     elves.reverse();
-
     elves[0]
 }
 
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &[Elf]) -> u32 {
     let mut elves = input.to_vec();
-    elves.sort();
-    elves.reverse();
-
-    elves[..3].iter().sum()
+    elves.sort_by(|a, b| b.cmp(a));
+    elves.iter().take(3).sum()
 }
